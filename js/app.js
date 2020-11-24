e423525908c3e042ebc3711c3198926a07de81c6
Oleg <oleg@localhost>
2020-11-24 17:31:43 +0700

    Conflicts are resolved

--- js/app.js
+++ js/app.js
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 (async () => {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
             const coordsEl = document.querySelector('#user-coords');
             coordsEl.textContent = `${position.coords.latitude}, ${position.coords.longitude}`
             coordsEl.parentElement.classList.remove('invisible');
-        });
-    }
-=======
-(() => {
     const earlyorderForm = document.querySelector('#earlyorder-form');
 
     earlyorderForm.addEventListener('submit', ev => {
@@ -28,9 +22,3 @@
             earlyorderContact.value = '';
         }, 500);
     });
->>>>>>> feature/earlyorder
-=======
-(async () => {
-    // TODO: add custom logic here
->>>>>>> 857e3d95477bfbd9c93fb04804a64d0007c1c075
-})();
